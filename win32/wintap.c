044878b770dd0931ef0bc8b8b3d9d80abdd5d029
lucktu <67133690@qq.com>
2026-05-13 06:04:54 +0000

    Network adapters in windows can be configured

--- win32/wintap.c
+++ win32/wintap.c
@@ -155,6 +155,8 @@ int tuntap_open(struct tuntap_dev *device, struct tuntap_config* config) {
     int i, err;
     ULONG status = TRUE;
     macstr_t mac_addr_buf;
+    int has_target = 0;
+    int target_tried = 0;
 
     memset(device, 0, sizeof(struct tuntap_dev));
     device->device_handle = INVALID_HANDLE_VALUE;
@@ -163,6 +165,7 @@ int tuntap_open(struct tuntap_dev *device, struct tuntap_config* config) {
 
     if (config->if_name && config->if_name[0] != '\0') {
         mbstowcs(adaptername_target, config->if_name, MAX_ADAPTER_NAME_LENGTH);
+        has_target = 1;
     }
 
     memset(&device->luid, 0, sizeof(NET_LUID));
@@ -192,145 +195,143 @@ int tuntap_open(struct tuntap_dev *device, struct tuntap_config* config) {
         if (err != 0)
             continue;
 
-        if(adaptername_target[0] != L'\0') {
-            if(!wcscmp(adaptername_target, adaptername)) {
-                found = 1;
-                break;
-            } else
+        /* If user specified a target adapter, try it first */
+        if (has_target && !target_tried) {
+            if (wcscmp(adaptername_target, adaptername))
                 continue;
+            target_tried = 1;
         }
 
+        /* Try to open this adapter */
         swprintf(tapname, sizeof(tapname), USERMODEDEVICEDIR L"%s" TAP_WIN_SUFFIX, adapterid);
-        device->device_handle = CreateFile(
+        HANDLE h = CreateFile(
             tapname, GENERIC_WRITE | GENERIC_READ,
-            0, /* Don't let other processes share or open the resource until the handle's been closed */
-            0, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM | FILE_FLAG_OVERLAPPED, 0
+            0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM | FILE_FLAG_OVERLAPPED, 0
         );
-        if(device->device_handle != INVALID_HANDLE_VALUE) {
-            found = 1;
-            break;
+        if (h == INVALID_HANDLE_VALUE) {
+            if (has_target) {
+                traceEvent(TRACE_WARNING, "Cannot open specified TAP '%ls', trying next...", adaptername_target);
+                has_target = 0;
+            }
+            continue;
         }
-    }
 
-    RegCloseKey(key);
+        /* Get MAC address */
+        uint8_t mac[6];
+        if (!DeviceIoControl(h, TAP_WIN_IOCTL_GET_MAC, mac, sizeof(mac), mac, sizeof(mac), &len, 0)) {
+            CloseHandle(h);
+            if (has_target) {
+                traceEvent(TRACE_WARNING, "Cannot get MAC from specified TAP '%ls', trying next...", adaptername_target);
+                has_target = 0;
+            }
+            continue;
+        }
 
-    if(!found) {
-        traceEvent(TRACE_ERROR, "No Windows TAP device found!");
-        exit(0);
-    }
-
-    /* ************************************** */
-
-    if(device->device_name[0] == '\0')
+        /* Save adapter info to device */
         wcscpy(device->device_name, adapterid);
+        device->device_handle = h;
+        memcpy(device->mac_addr, mac, sizeof(mac));
 
-     if(device->ifIdx == NET_IFINDEX_UNSPECIFIED) {
         if (get_adapter_luid(adapterid, &device->luid) == 0) {
             IF_INDEX index = NET_IFINDEX_UNSPECIFIED;
             if (ConvertInterfaceLuidToIndex(&device->luid, &index) == 0) {
                 device->ifIdx = index;
             }
         }
-    }
 
-    /* Try to open the corresponding tap device->device_name */
+        memcpy(&device->ip_addr, &config->ip_addr, sizeof(config->ip_addr));
+        device->ip_prefixlen = config->ip_prefixlen;
+        memcpy(&device->ip6_addr, &config->ip6_addr, sizeof(config->ip6_addr));
+        device->ip6_prefixlen = config->ip6_prefixlen;
+        device->mtu = config->mtu;
+        device->dyn_ip4 = config->dyn_ip4;
+        device->routes = config->routes;
+        device->routes_count = config->routes_count;
 
-    if(device->device_handle == INVALID_HANDLE_VALUE) {
-        swprintf(tapname, sizeof(tapname), USERMODEDEVICEDIR "%s" TAP_WIN_SUFFIX, device->device_name);
-        device->device_handle = CreateFile(
-            tapname, GENERIC_WRITE | GENERIC_READ, 0, 0,
-            OPEN_EXISTING, FILE_ATTRIBUTE_SYSTEM | FILE_FLAG_OVERLAPPED, 0
-        );
-    }
+        traceEvent(TRACE_NORMAL, "Interface %ls has MAC %s", adaptername, macaddr_str(mac_addr_buf, device->mac_addr));
 
-    if(device->device_handle == INVALID_HANDLE_VALUE) {
-        traceEvent(TRACE_ERROR, "%ls is not a usable Windows TAP device", adaptername);
-        exit(-1);
-    }
-
-    /* Get MAC address from tap device->device_name */
-    if(!DeviceIoControl(
-        device->device_handle, TAP_WIN_IOCTL_GET_MAC,
-        device->mac_addr, sizeof(device->mac_addr),
-        device->mac_addr, sizeof(device->mac_addr), &len, 0)
-    ) {
-        traceEvent(TRACE_ERROR, "Could not get MAC address from Windows TAP %ls", adaptername);
-        return -1;
-    }
-
-    memcpy(&device->ip_addr, &config->ip_addr, sizeof(config->ip_addr));
-    device->ip_prefixlen = config->ip_prefixlen;
-    memcpy(&device->ip6_addr, &config->ip6_addr, sizeof(config->ip6_addr));
-    device->ip6_prefixlen = config->ip6_prefixlen;
-    device->mtu = config->mtu;
-    device->dyn_ip4 = config->dyn_ip4;
-    device->routes = config->routes;
-    device->routes_count = config->routes_count;
-
-    traceEvent(TRACE_NORMAL, "Interface %ls has MAC %s", adaptername, macaddr_str(mac_addr_buf, device->mac_addr));
-
-    /* ****************** */
-
-    /* Delay IP configuration on Windows when requested and IP is not yet known */
-    if (config->delay_ip_config && device->ip_addr == 0) {
-        traceEvent(TRACE_NORMAL, "Delaying IP configuration until REGISTER_SUPER_ACK");
-        rc = 0;
-    } else {
-        if (device->dyn_ip4) {
-            rc = set_dhcp(device);
+        /* Configure IP address */
+        if (config->delay_ip_config && device->ip_addr == 0) {
+            traceEvent(TRACE_NORMAL, "Delaying IP configuration until REGISTER_SUPER_ACK");
+            rc = 0;
         } else {
-            rc = set_static_ip_address(device);
+            if (device->dyn_ip4) {
+                rc = set_dhcp(device);
+            } else {
+                rc = set_static_ip_address(device);
+            }
         }
-    }
-
-    if (rc != 0) {
-        W32_ERROR(rc, error)
-        traceEvent(TRACE_WARNING, "Unable to set device %ls IP address: %u", adaptername, error);
-        W32_ERROR_FREE(error)
-
-        return -1;
-    }
-
-    if (device->routes_count > 0) {
-        rc = set_static_routes(device);
 
         if (rc != 0) {
             W32_ERROR(rc, error)
-            traceEvent(TRACE_WARNING, "Unable to set device %ls static route: %u", adaptername, error);
+            traceEvent(TRACE_WARNING, "Unable to set device %ls IP address: %u", adaptername, error);
             W32_ERROR_FREE(error)
-
-            return -1;
+            CloseHandle(device->device_handle);
+            device->device_handle = INVALID_HANDLE_VALUE;
+            if (has_target) {
+                traceEvent(TRACE_WARNING, "Specified TAP '%ls' failed, trying next...", adaptername_target);
+                has_target = 0;
+            }
+            continue;
         }
+
+        if (device->routes_count > 0) {
+            rc = set_static_routes(device);
+            if (rc != 0) {
+                W32_ERROR(rc, error)
+                traceEvent(TRACE_WARNING, "Unable to set device %ls static route: %u", adaptername, error);
+                W32_ERROR_FREE(error)
+                CloseHandle(device->device_handle);
+                device->device_handle = INVALID_HANDLE_VALUE;
+                if (has_target) {
+                    has_target = 0;
+                }
+                continue;
+            }
+        }
+
+        tuntap_get_address(device);
+
+        if(device->mtu != DEFAULT_MTU)
+            traceEvent(TRACE_WARNING, "MTU set is not supported on Windows");
+
+        /* set driver media status to 'connected' */
+        if (!DeviceIoControl(
+            device->device_handle, TAP_WIN_IOCTL_SET_MEDIA_STATUS,
+            &status, sizeof (status),
+            &status, sizeof (status), &len, NULL
+        )) {
+            W32_ERROR(GetLastError(), error)
+            traceEvent(TRACE_ERROR, "Unable to enable TAP adapter %ls: %ls", adaptername, error);
+            W32_ERROR_FREE(error)
+        }
+
+        /*
+        * Initialize overlapped structures
+        */
+        device->overlap_read.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+        device->overlap_write.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+        if (!device->overlap_read.hEvent || !device->overlap_write.hEvent) {
+            CloseHandle(device->device_handle);
+            device->device_handle = INVALID_HANDLE_VALUE;
+            continue;
+        }
+
+        InitializeCriticalSection(&device->write_lock);
+
+        found = 1;
+        break;
     }
 
-    tuntap_get_address(device);
+    RegCloseKey(key);
 
-    /* ****************** */
-
-    if(device->mtu != DEFAULT_MTU)
-        traceEvent(TRACE_WARNING, "MTU set is not supported on Windows");
-
-    /* set driver media status to 'connected' (i.e. set the interface up) */
-    if (!DeviceIoControl(
-        device->device_handle, TAP_WIN_IOCTL_SET_MEDIA_STATUS,
-        &status, sizeof (status),
-        &status, sizeof (status), &len, NULL
-    )) {
-        W32_ERROR(GetLastError(), error)
-        traceEvent(TRACE_ERROR, "Unable to enable TAP adapter %ls: %ls", adaptername, error);
-        W32_ERROR_FREE(error)
+    if (!found) {
+        if (adaptername_target[0] != L'\0')
+            traceEvent(TRACE_ERROR, "Specified TAP device '%ls' not found or not usable, and no fallback available!", adaptername_target);
+        else
+            traceEvent(TRACE_ERROR, "No Windows TAP device found!");
+        exit(0);
     }
-
-    /*
-    * Initialize overlapped structures
-    */
-    device->overlap_read.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
-    device->overlap_write.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
-    if (!device->overlap_read.hEvent || !device->overlap_write.hEvent) {
-        return -1;
-    }
-
-    InitializeCriticalSection(&device->write_lock);
 
     return 0;
 }
